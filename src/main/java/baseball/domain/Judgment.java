538b6301421be225c97c209b064b24bb64090dbe
kissshot1104 <kissshot.heartunderblade1104@gmail.com>
2022-11-09 23:27:01 +0900

    Feat : 특정 자리에 특정 숫자가 있는지 알 수 있는 기능 구현

--- src/main/java/baseball/domain/Judgment.java
+++ src/main/java/baseball/domain/Judgment.java
@@ -14,7 +14,8 @@ public class Judgment {
         return result;
     }
 
-    public boolean hasPlace(int placeNumber, int number){
-        return false;
+    public boolean hasPlace(List<Integer> computer, int placeIndex, int number){
+        //if (computer.get(placeIndex) == number)
+        return computer.get(placeIndex) == number;
     }
 }
